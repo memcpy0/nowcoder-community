beba7d2786fd993f94a89d3fd288ff24063af91e
memcpy0 <2183927003@qq.com>
2023-04-25 18:17:26 +0800

    核心功能-显示帖子详情下的评论和回复

---
+++ src/main/java/com/nowcoder/community/constant/EntityTypes.java
@@ -0,0 +1,12 @@
+package com.nowcoder.community.constant;
+
+public class EntityTypes {
+    /**
+     * 实体类型：帖子
+     */
+    public final static int ENTITY_TYPE_POST = 1;
+    /**
+     * 实体类型：评论
+     */
+    public final static int ENTITY_TYPE_COMMENT = 2;
+}
